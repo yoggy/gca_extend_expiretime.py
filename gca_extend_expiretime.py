61704a53c79a959b0a4c6e1d33e35b7b55dbbbf2
yoggy <yoggy0@gmail.com>
2023-01-17 12:36:03 +0900

    ...

--- gca_extend_expiretime.py
+++ gca_extend_expiretime.py
@@ -30,7 +30,7 @@ gca_list = json.loads(gca_list_str)
 for gca in gca_list["anchors"]:
   anchor_id = gca["name"].split("/")[1]  #name="anchor/ua-xxxxxxxxxxxxxxxx"
 
-  rv = subprocess.check_output(["curl","-H",f"Authorization: Bearer {bearer_token}","-H","Content-Type: application/json","-X","PATCH",f"https://arcore.googleapis.com/v1beta2/management/anchors/{anchor_id}?updateMask=expire_time","-d","{expireTime:\"2999-12-31T00:00:00Z\"}"],stderr=subprocess.DEVNULL)
+  rv = subprocess.check_output(["curl","-H",f"Authorization: Bearer {bearer_token}","-H","Content-Type: application/json","-X","PATCH",f"https://arcore.googleapis.com/v1beta2/management/anchors/{anchor_id}?updateMask=expire_time","-d","{expireTime:\"9999-12-31T00:00:00Z\"}"],stderr=subprocess.DEVNULL)
 
   print(rv)
 
